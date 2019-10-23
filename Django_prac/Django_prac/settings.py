56f2fb445bf14650e14ca8da3b3d10c81c860697
KIngSimeone <53562471+KIngSimeone@users.noreply.github.com>
2019-10-23 13:57:39 +0100

    Initialized apllication and created database

--- Django_prac/Django_prac/settings.py
+++ Django_prac/Django_prac/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'article',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'Django_prac.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'C:/Users/simeo/Documents/Simeon/Django Practice/Django_prac/storage.db'
     }
 }
 
