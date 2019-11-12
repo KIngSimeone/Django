8f1bbe8539073ed63b16cc47de8461996cc65440
KIngSimeone <53562471+KIngSimeone@users.noreply.github.com>
2019-11-12 16:25:16 +0100

    Detail View and List View 1

--- Learn Django/advanced_section/advcbv/basic_app/views.py
+++ Learn Django/advanced_section/advcbv/basic_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View,TemplateView
-
+from django.views.generic import ListView,DetailView
+from . import models
 
 # Create your views here.
-class IndexView(TemplateView):
-    template_name = 'index.html'
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'BASIC INJECTION'
-        return context
+class SchoolListView(ListView):
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
         
         
  
